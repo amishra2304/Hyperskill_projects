bf36180db51da7bb28dbcaa853d591c7d87694ad
amishra2304 <amishra@griddynamics.com>
2024-02-01 15:51:23 +0530

    Added Implementation code.

--- com/LeaningJava/RaceAndSync.java
+++ com/LeaningJava/RaceAndSync.java
@@ -1,7 +1,7 @@
 package com.LeaningJava;
 class counter{
     int count;
-    public void increment(){
+    public synchronized void increment(){
         count++;
     }
 }
@@ -24,10 +24,27 @@ Thread t1=new Thread(obj1);
 Thread t2=new Thread(obj2);
 t1.start();
 t2.start();
-//the join statement finally joins the both thread thus
-        //making sure tha
+//but even if we had started the new thread now but
+        //when we have a shared variable and we increment
+        // 2 times then increment happens only once..
+        //ex: count++ => count=count+1;
+        //because both the thread reaches the variable at
+        //same time
+        //in that case if we want that only one thread approaches
+        //shared variables at a time.
+//what is join? main started the threads and without
+        //waiting for threads to complete it just executes
+        //next statement of printing the count
+        // in between jitta bhi increment ho paya
+        //usse main ko mtlb nhi..
+        //so we use a join method which shows that wait until
+        //threads complete the execution and joins back the
+        //main method
 t1.join();
 t2.join();
+
+//joins plus synchronize is equal to constant output
+
         System.out.println(c.count);
     }
 }
